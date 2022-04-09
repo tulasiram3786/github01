bd42fbe5cdb75a120d6a7d109d88351a86bcf7d5
tulasiram3786 <102223593+tulasiram3786@users.noreply.github.com>
2022-04-09 19:23:30 +0530

    Update DemoTest.java

--- metro/src/test/java/sampleTest/DemoTest.java
+++ metro/src/test/java/sampleTest/DemoTest.java
@@ -5,5 +5,9 @@ public class DemoTest {
 	public void CreateOrgn() {
 		System.out.println("organization is created");
 	}
+	public void CreateOrgn1() {
+		System.out.println("organization is created");
+	}
+
 
 }
